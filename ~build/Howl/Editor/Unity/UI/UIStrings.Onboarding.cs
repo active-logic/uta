16369865a8d660d1d7d2eec52cf88d1b16754093
Tea <tea@1k1.io>
2020-08-12 12:52:32 +0800

    Fmt

--- ~build/Howl/Editor/Unity/UI/UIStrings.Onboarding.cs
+++ ~build/Howl/Editor/Unity/UI/UIStrings.Onboarding.cs
@@ -1,6 +1,6 @@
 namespace Active.Howl.UIStrings{ public static class Onboarding{
 
-    public const string GetIDE       =
+    public const string GetIDE =
 @"A supported IDE with well configured
 snippets is required; Atom is recommended.";
 
@@ -9,16 +9,18 @@ snippets is required; Atom is recommended.";
 syntax coloring in Atom
 [Restart Atom after installing]";
 
-    public const string ImportFiles  =
+    public const string ImportFiles =
 @"Convert your C# scripts to Howl?
 If you do not wish to use all notations,
 you can do this later";
 
-    public const string SetupVCS     =
-"Ensure your project is using version control\n(required during β)";
+    public const string SetupVCS =
+@"Ensure your project is using version control
+(required during β)";
 
     public const string AllDone      =
-"All is well; access documentation using\nthe hereabove [?] badge ~ ";
+@"All is well; access documentation using
+the hereabove [?] badge ~ ";
 
     public const string Okay         = "╰(*´︶`*)╯ OK!";
 
